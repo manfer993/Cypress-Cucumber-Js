4f8d3e60097e162a136c00e001396eef1b1717c7
Manuel Munoz <Manuel.Munoz.-ND@disney.com>
2023-07-14 10:31:21 -0500

    remove temp duplicate function

--- cypress/e2e/step_definitions/cross.js
+++ cypress/e2e/step_definitions/cross.js
@@ -4,7 +4,3 @@ import { crossPage } from "@pages/crossPage";
 When("The platform page has {string} title", (pageTitle) => {
   crossPage.getPageTitle().should("have.text", pageTitle);
 });
-
-When("The platform page has {string} title", (pageTitle) => {
-  crossPage.getPageTitle().should("have.text", pageTitle);
-});
